44b00a1207b06f3ad695a39eb7c71fce335afec4
Gabriel Sanches <gabriel@gsr.dev>
2021-03-24 12:34:15 -0300

    Skip whitespace patch for icons not found

--- lua/nvim-nonicons/util.lua
+++ lua/nvim-nonicons/util.lua
@@ -2,7 +2,8 @@ local icons = require("nvim-nonicons.mapping")
 
 local function get(name)
   local icon = vim.fn.nr2char(icons[name])
-  if not vim.g.nonicons_pad_right then
+
+  if icon == '' or not vim.g.nonicons_pad_right then
     return icon
   end
 
